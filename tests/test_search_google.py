79e7b6a0736fd9bc48493f49a30620d6171ee9f8
Baslandze Lasha <lasha.qa@mail.ru>
2023-04-12 18:38:03 +0300

    update fixture

---
+++ tests/test_search_google.py
@@ -0,0 +1,19 @@
+import time
+
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(autouse=True)
+def open_browser():
+    browser.open('https://google.com')
+    yield browser
+
+
+
+@pytest.fixture(scope='session')
+def size_browser():
+    browser.config.window_width = 700
+    browser.config.window_height = 500
+    time.sleep(3)
+    browser.quit()
